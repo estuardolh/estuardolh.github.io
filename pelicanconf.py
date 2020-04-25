c0f60817a3f92fca56d472ab3fc9e768c4d1eec4
Estuardo López <estuardolh@gmail.com>
2020-04-24 21:13:30 -0600

    base source

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+THEME = "/home/estuardolh/pelican-themes/taman/"
+
+AUTHOR = u'Estuardo López'
+SITENAME = u'Estuardo info'
+SITEURL = ''
+DESCRIPTION = u'José Estuardo López H. </br> Programming lover / Developer'
+PATH = 'content'
+
+TIMEZONE = 'America/Guatemala'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('', ''),)
+
+# Social widget
+SOCIAL = ((),)
+
+DEFAULT_PAGINATION = 6
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
