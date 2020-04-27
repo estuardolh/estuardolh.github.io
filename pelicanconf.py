ff2db7771697142544606634621f702a0a721a30
Estuardo López <estuardolh@gmail.com>
2020-04-26 19:06:04 -0600

    favicon and nav tool

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,14 +7,15 @@ THEME = "/home/estuardolh/pelican-themes/taman/"
 AUTHOR = u'Estuardo López'
 SITENAME = u'Estuardo info'
 SITEURL = ''
-DESCRIPTION = u'José Estuardo López H. </br> Programming lover / Developer'
+DESCRIPTION = u'Estuardo López'
 PATH = 'content'
 
 TIMEZONE = 'America/Guatemala'
 
 DEFAULT_LANG = u'en'
-
+#A programming lover.
 # Feed generation is usually not desired when developing
+BIO = u'<h3>ESTUARDO LÓPEZ</h3><p>Engineering student and programming lover.</p>'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -23,7 +24,10 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('', ''),)
-
+SECTIONS = (('🏡','https://estuardo.info')
+  ,('Resume', '/resume.pdf')
+  , ('Repos', 'https://github.com/estuardolh')
+  , ('Bio', '/bio'),)
 # Social widget
 SOCIAL = ((),)
 
